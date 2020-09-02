666cecf3296710973cccea3aeb8e57845e2de17c
Nguyen Huu Khanh <nguyenkhanh87@gmail.com>
2020-09-02 13:42:19 +0700

    Update 02/09/2020

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 class StaffController extends Controller
 {
     public function index(Request $request) {
-        $numberOfItems = 12;
+        $numberOfItems = 10;
         $items = DB::connection('mysql2')->table("mvup0_mysite_giaovien")
                     ->join('mvup0_users','mvup0_mysite_giaovien.user_id','=','mvup0_users.id','inner')
                     ->whereRaw("mvup0_mysite_giaovien.user_id IN (SELECT user_id FROM mvup0_mysite_khoa_giaovien WHERE khoa_id=20)")
--- resources/views/classic/staff.blade.php
+++ resources/views/classic/staff.blade.php
@@ -12,12 +12,12 @@
     <div class="tab-body">
         <div class="row">
             @foreach ($items as $item)
-                <div class="mb-2 col-6">
+                <div class="mb-2 col-md-6">
                     <div class="item border">
                         <div class="row">
-                            <div class="col-3"><img class="img-fluid" src="{{ empty($item->image)?'https://image.flaticon.com/icons/svg/906/906175.svg':'http://mysite.tnu.edu.vn/'.$item->image }}" alt="{{ $item->name }}"></div>
+                            <div class="col-3"><a class="text-primary" target="_blank" href="http://mysite.tnu.edu.vn/vi/{{ $item->alias }}"><img class="img-fluid" src="{{ empty($item->image)?'https://image.flaticon.com/icons/svg/906/906175.svg':'http://mysite.tnu.edu.vn/'.$item->image }}" alt="{{ $item->name }}"></a></div>
                             <div class="col-9">
-                                <h5>{{ $item->name }}</h5>
+                                <h5 class="mb-0"><a class="text-primary" target="_blank" href="http://mysite.tnu.edu.vn/vi/{{ $item->alias }}">{{ $item->name }}</a></h5>
                                 @if ($item->hocvi)
                                     <p class="mb-0">Học vị: {{ $item->hocvi }}</p>
                                 @endif
